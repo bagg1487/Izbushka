bcc7e5f69f77ae14f28824ab60505621ab26e0cd
Пётр Нгуен <petya.nguen@bk.ru>
2026-04-26 19:23:37 +0300

    Clear project STM32

--- Control/Control PC Web/app/websocket/__init__.py
+++
@@ -1,1 +0,0 @@
-from .video_stream import video_manager
--- Other/STM32Arduino/ultrasonic.h
+++
@@ -1,9 +0,0 @@
-#ifndef ULTRASONIC_H
-#define ULTRASONIC_H
-
-#include "config.h"
-
-void setupUltrasonic();
-float measureDistance();
-
-#endif
